c17b5d30540f24126164565fd9a1dc77c63a5e9c
Mo7amded Tamer <mtamer.infosec@gmail.com>
2025-10-09 18:21:49 +0300

    Python Project

---
+++ Python/Problems_Solving/fine_the_missing_letter_01.py
@@ -0,0 +1,24 @@
+import string
+# letters = string.ascii_letters
+
+# def main(user_input):
+#     first_char = user_input[0]
+#     last_char  = user_input[-1]
+
+#     start = letters.index(first_char)
+#     end = letters.index(last_char)
+
+#     missing = []
+#     for i in range(start, end + 1):
+#         if letters[i] not in user_input:
+#             missing.append(letters[i])   # ضيف أي حرف ناقص
+
+#     return missing if missing else None  # لو ناقص رجّعهم، لو كله تمام رجّع None
+
+# print(main('fghjk'))   # ['i']
+# print(main('fghk'))    # ['i', 'j']
+# print(main('abcdef'))  # None
+
+# =====================================
+
+
