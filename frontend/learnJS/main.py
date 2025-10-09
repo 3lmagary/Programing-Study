0739593f831303472f4c2acd0b6d64353dc032f4
Mo7amded Tamer <mtamer.infosec@gmail.com>
2025-10-09 19:14:01 +0300

    edit

---
+++ frontend/learnJS/main.py
@@ -0,0 +1,18 @@
+# models = [2020, 2024, 2025, 2030]
+# cars = ['BMW', 'Mercedes', 'Tesla']
+
+# for car in cars:
+#     print(f"Cars: {car}")
+#     print("-" * 10)
+#     for model in models:
+#         print(f"Model: {model}")
+
+
+# /////////////////////
+
+def main(age):
+    result = age * 365
+    return result
+
+
+print(main(18))
